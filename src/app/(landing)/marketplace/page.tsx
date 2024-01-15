e94df30abad7e9bbf509d3421d020d7ac38cc038
davidmedev222 <davidmedev@gmail.com>
2024-01-15 17:15:46 -0300

    feat(ui): add UI of the browse marketplace section

---
+++ src/app/(landing)/marketplace/page.tsx
@@ -0,0 +1,11 @@
+import { BrowseMarketplaceSection } from '@/components'
+
+function MarketplacePage() {
+  return (
+    <main>
+      <BrowseMarketplaceSection />
+    </main>
+  )
+}
+
+export default MarketplacePage
